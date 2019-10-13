8ee55e12d7aea38cac162bc58f1c63d4e7dc3db8
Matumo <dev@matumo.com>
2019-10-14 02:01:51 +0900

    bcコマンドを使用しないよう変更

--- ShellScript/setTime.sh
+++ ShellScript/setTime.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
 sudo date > /dev/null
-start_time=`date +%s.%3N`
-server_time=`wget -q https://ntp-a1.nict.go.jp/cgi-bin/jst -O - | sed -n 4p`
-end_time=`date +%s.%3N`
-lag=`echo "scale=3; ($end_time - $start_time)/2.0" | bc`
-time=`echo "scale=3; $server_time + $lag" | bc`
+
+start_time=`date +%s%3N`
+server_time=`wget -q http://ntp-a1.nict.go.jp/cgi-bin/jst -O - | sed -n 4p`
+end_time=`date +%s%3N`
+lag=`echo $(($end_time - $start_time))`
+time=`echo $(($(echo $server_time | sed 's/\.//') + $lag))`
+lag=`echo $lag | sed 's/\([0-9]\{1,3\}\)$/.\1/'`
+time=`echo $time | sed 's/\([0-9]\{1,3\}\)$/.\1/'`
+
 echo "Get Time $server_time"
 echo "Lag Time $lag"
 echo "Set Time $time"
+
 sudo date -s @"$time"
