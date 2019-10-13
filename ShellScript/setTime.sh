cb0017924136b9d9e42ed5f649b33cb4148cd10c
Matumo <dev@matumo.com>
2019-10-14 00:58:53 +0900

    Fix bug

--- ShellScript/setTime.sh
+++ ShellScript/setTime.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo date -s @"$(wget -q https://ntp-a1.nict.go.jp/cgi-bin/jst -O - | awk -F 'NR==4{print $0}')"
+sudo date -s @"$(wget -q https://ntp-a1.nict.go.jp/cgi-bin/jst -O - | awk 'NR==4{print $0}')"
