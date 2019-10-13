66f4a74b22645d48a4b7d767d09998e653c54c9b
Matumo <dev@matumo.com>
2019-10-14 01:00:44 +0900

    タイムラグを考慮するよう変更

--- ShellScript/setTime.sh
+++ ShellScript/setTime.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo date -s @"$(wget -q https://ntp-a1.nict.go.jp/cgi-bin/jst -O - | awk 'NR==4{print $0}')"
+sudo date -s @"$(wget -q https://ntp-a1.nict.go.jp/cgi-bin/jst -O - | awk 'NR==4{OFMT="%.6f";print $0+0.1}')"
