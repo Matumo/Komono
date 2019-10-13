e8252f7643ed56f7b0819352f9c65339f60f6ac8
Matumo <dev@matumo.com>
2019-10-14 01:30:39 +0900

    sudoのパスワード認証を事前にクリアするよう変更，タイムラグを比較的正確に計算するよう変更，ログの表示を追加

--- ShellScript/setTime.sh
+++ ShellScript/setTime.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-sudo date -s @"$(wget -q https://ntp-a1.nict.go.jp/cgi-bin/jst -O - | awk 'NR==4{OFMT="%.6f";print $0+0.1}')"
+sudo date > /dev/null
+start_time=`date +%s.%3N`
+server_time=`wget -q https://ntp-a1.nict.go.jp/cgi-bin/jst -O - | sed -n 4p`
+end_time=`date +%s.%3N`
+lag=`echo "scale=3; ($end_time - $start_time)/2.0" | bc`
+time=`echo "scale=3; $server_time + $lag" | bc`
+echo "Get Time $server_time"
+echo "Lag Time $lag"
+echo "Set Time $time"
+sudo date -s @"$time"
